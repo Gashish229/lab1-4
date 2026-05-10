c00d803e234f2037f43b4a214e8715b550bde50a
Gashish229 <SINKOV07@BK.RU>
2026-05-10 17:35:59 +0700

    Update bitrgbled.c

--- bitrgbled.c
+++ bitrgbled.c
@@ -1,5 +1,6 @@
 #include "bitrgbled.h"
 #include <stdlib.h>
+#include <stdio.h> // Добавили для printf и scanf
 
 // Выделяем память под один пиксель и заполняем его данными
 rgbled* create_pixel(unsigned int r, unsigned int g, unsigned int b, unsigned int mode) {
@@ -7,11 +8,26 @@ rgbled* create_pixel(unsigned int r, unsigned int g, unsigned int b, unsigned in
     pixel->red = r;
     pixel->green = g;
     pixel->blue = b;
-    pixel->brightness = 100; // Яркость на максимум по умолчанию
-    pixel->temp = 6500;      // Стандартный белый свет
+    pixel->brightness = 100; 
+    pixel->temp = 6500;      
     pixel->module_type = 1;  
     pixel->mode = mode;      
     return pixel;
+}
+
+// Метод для ручного заполнения структуры (Требование Лабы №2)
+void fill_pixel_manual(rgbled* p) {
+    if (p == NULL) return;
+    
+    int r, g, b, mode;
+    printf("Введите через пробел R, G, B (0-255) и Режим (0-2): ");
+    scanf("%d %d %d %d", &r, &g, &b, &mode);
+    
+    // Безопасно присваиваем считанные int значения нашим битовым полям
+    p->red = r;
+    p->green = g;
+    p->blue = b;
+    p->mode = mode;
 }
 
 // Освобождаем память
